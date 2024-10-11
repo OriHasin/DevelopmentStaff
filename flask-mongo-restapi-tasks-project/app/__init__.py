ded7cded11da7e0ed88c057037f36fbf041a5349
Ori Hasin <orihasin@Oris-MacBook-Pro.local>
2024-10-11 15:09:01 +0300

    adding middlewares

--- flask-mongo-restapi-tasks-project/app/__init__.py
+++ flask-mongo-restapi-tasks-project/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from .extensions import mongo
 from .views import main
 def create_app(config_object):
@@ -18,5 +18,15 @@ def create_app(config_object):
         message = {"Message": "An Error Occurred!",
                    "Error": str(error)}
         return jsonify(message), 500
+
+
+    @app.before_request
+    def before_request():
+        print(f'Request arrived from ip: {request.remote_addr}, method: {request.method}, path: {request.path}')
+
+    @app.after_request
+    def after_request(response):
+        print(f'Responding to request: {request.method} {request.path} with status code: {response.status_code}')
+        return response
 
     return app
