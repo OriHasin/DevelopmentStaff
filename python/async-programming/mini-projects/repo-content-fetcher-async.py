713f2676f531aa4c608f0dbfbfb8393eae05e749
Ori Hasin <orihasin@Oris-MacBook-Pro.local>
2025-01-29 21:16:21 +0200

    adding RepoContentFetcher inside pythom/async_programming

--- python/async-programming/mini-projects/repo-content-fetcher-async.py
+++ python/async-programming/mini-projects/repo-content-fetcher-async.py
@@ -1,1 +1,44 @@
+import asyncio
+import os
+import time
 import aiohttp
+import dotenv
+import pprint
+
+dotenv.load_dotenv(".env")
+
+
+async def get_repo_content(owner, repo, path="/"):
+    async with aiohttp.ClientSession() as session:
+        return await _get_repo_content(session, owner, repo, path)
+
+
+async def _get_repo_content(session, owner, repo, path="/"):
+    async def _process_data(element):
+        if element["type"] == "file":
+            return {"type": element["type"], "name": element["name"]}
+
+        elif element["type"] == "dir":
+            return {"children": await _get_repo_content(session, owner, repo, element["path"]), "path": element["path"],
+                    "type": element["type"], "name": element["name"]}
+
+
+    url = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
+    async with session.get(url, headers={"Authorization": f"token {os.environ.get('TOKEN')}"}) as response:
+        data = await response.json()
+
+    current_level = [asyncio.create_task(_process_data(element)) for element in data]
+    results = await asyncio.gather(*current_level)
+
+    return results
+
+
+
+
+
+
+if __name__ == '__main__':
+    start_time = time.monotonic()
+    pprint.pprint(asyncio.run(get_repo_content("OriHasin", "DevelopmentStaff")))
+    end_time = time.monotonic()
+    print(f"Total Execution Time: {end_time - start_time} seconds")
--- python/async-programming/mini-projects/repo-content-fetcher.py
+++ python/async-programming/mini-projects/repo-content-fetcher.py
@@ -1,47 +1,31 @@
 import os
-
 import requests
-import pprint
 import dotenv
+import pprint
+import time
 
 dotenv.load_dotenv(".env")
 
-class FileNode:
-    def __init__(self, name, download_url):
-        self.name = name
-        self.download_url = download_url
-
-    def __repr__(self):
-        return f"name: {self.name}"
-
-
-class DirNode:
-    def __init__(self, name, path):
-        self.name = name
-        self.path = path
-        self.children = None
-    def __repr__(self):
-        return f"name: {self.name}, path: {self.path}, childs: {self.children}"
-
-
-
-
-
 
 def get_repo_content(owner, repo, path="/"):
-    URL = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
-    response = requests.get(URL, headers={"Authorization": f"token {os.environ.get('TOKEN')}"}).json()
+    url = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
+    response = requests.get(url, headers={"Authorization": f"token {os.environ.get('TOKEN')}"}).json()
     current_level = []
+
     for element in response:
+
         if element["type"] == "file":
-            current_level.append(FileNode(element["name"], element["download_url"]))
+            current_level.append({"type": element["type"], "name": element["name"]})
+
         elif element["type"] == "dir":
-            curr_dir = DirNode(element["name"], element["path"])
-            curr_dir.children = get_repo_content(owner, repo, element["path"])
+            curr_dir = {"children": get_repo_content(owner, repo, element["path"]), "path": element["path"], "type": element["type"], "name": element["name"]}
             current_level.append(curr_dir)
+
     return current_level
 
 
-
 if __name__ == '__main__':
+    start_time = time.monotonic()
     pprint.pprint(get_repo_content("OriHasin", "DevelopmentStaff"))
+    end_time = time.monotonic()
+    print(f"Total Execution Time: {end_time - start_time} seconds")
